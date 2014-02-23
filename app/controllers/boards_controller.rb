6c1e5059e5d9882975c3c575769003e436df0c17
Nariki Yasutaka <prototype099@gmail.com>
2014-02-23 20:20:37 +0900

    show users facebook icon image

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,8 @@ class BoardsController < ApplicationController
   before_filter :detect_event
 
   def index
-    @tasks = Task.where(event_id: @event.id)
+    @tasks = Task.includes(:user).includes(:user => :oauth_tokens).where(event_id: @event.id)
+
     @task = Task.new(event_id: @event.id)
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+  def fb_icon(user)
+    user_oauth_token = user.oauth_tokens.detect { |oauth_token| oauth_token.provider == "facebook" }
+    if user_oauth_token
+      image_tag "https://graph.facebook.com/#{user_oauth_token.uid}/picture?type=square", alt: user.name
+    else
+      'no_image'
+    end
+  end
+
 end
